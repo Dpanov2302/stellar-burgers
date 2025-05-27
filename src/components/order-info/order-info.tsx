28615f4974ce50c5ed618264a508fdb04b423968
Dpanov2302 <dpanov2302@gmail.com>
2025-05-27 16:50:21 +0300

    Make burger constructor and storage, profile and auth page

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,21 +1,30 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { RootState, useDispatch, useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
+import { fetchOrderByNumber } from '../../services/slices/orders-slice';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const { number } = useParams<{ number: string }>();
 
-  const ingredients: TIngredient[] = [];
+  const dispatch = useDispatch();
+
+  /** TODO: взять переменные orderData и ingredients из стора */
+  const orderData = useSelector(
+    (state: RootState) => state.orders.currentOrder
+  );
+
+  const ingredients: TIngredient[] = useSelector(
+    (state: RootState) => state.ingredients.items
+  );
+
+  useEffect(() => {
+    if (!orderData || orderData.number.toString() !== number) {
+      dispatch(fetchOrderByNumber(Number(number)));
+    }
+  }, [dispatch, number, orderData]);
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
